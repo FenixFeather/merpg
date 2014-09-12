531038695519e304becdc90201188c4ef7a4007a
Thomas Liu <thomasliu02@gmail.com>
2014-09-12 17:30:19 -0500

    Use the package instead of including the file

--- client.jl
+++ client.jl
@@ -1,9 +1,10 @@
 #!/usr/bin/env julia
 include("actors.jl")
 include("map.jl")
-include("InputStuff.jl")
+using InputStuff
 
 using InputStuff
+using JSON
 
 function main()
     the_map = Map([Cell(Actor[]) for x in 1:3, y in 1:3])
@@ -22,6 +23,7 @@ function main()
         display[shepard.position...] = " "
         bla = input("> ")
         move!(shepard, the_map, move_dict[bla])
+        println(JSON.json(the_map))
     end
 
     
